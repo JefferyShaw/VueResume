09a8fd357cef8132a351b33c34148ae9e0bdd9b6
shaw <zhenhui.xiao@qq.com>
2019-07-10 22:33:02 +0800

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,245 @@
+<template>
+  <div id="app">
+    <StyleEditor ref="styleEditor" :code="currentStyle"></StyleEditor>
+    <ResumeEditor ref="resumeEditor" :markdown="currentMarkdown" :enableHtml="enableHtml"></ResumeEditor>
+  </div>
+</template>
+
+<script>
+  import StyleEditor from './components/StyleEditor'
+  import ResumeEditor from './components/ResumeEditor'
+  import './assets/reset.css'
+
+  export default {
+    name: 'app',
+    components: {
+      StyleEditor,
+      ResumeEditor
+    },
+    data() {
+      return {
+        interval: 40,
+        currentStyle: '',
+        enableHtml: false,
+        fullStyle: [
+          `/*
+* 大家好，我是肖振辉
+* 这是我的Vue简历
+*/
+
+/* 首先给所有元素加上过渡效果 */
+* {
+  transition: all .3s;
+}
+/* 白色背景太单调了，我们来点背景 */
+html {
+  color: rgb(222,222,222); background: rgb(0,43,54);
+}
+/* 文字离边框太近了 */
+.styleEditor {
+  padding: .5em;
+  border: 1px solid;
+  margin: .5em;
+  overflow: auto;
+  width: 45vw; height: 90vh;
+}
+/* 代码高亮 */
+.token.selector{ color: rgb(133,153,0); }
+.token.property{ color: rgb(187,137,0); }
+.token.punctuation{ color: yellow; }
+.token.function{ color: rgb(42,161,152); }
+
+/* 加点 3D 效果呗 */
+html{
+  perspective: 1000px;
+}
+.styleEditor {
+  position: fixed; left: 0; top: 0;
+  -webkit-transition: none;
+  transition: none;
+  -webkit-transform: rotateY(10deg) translateZ(-100px) ;
+          transform: rotateY(10deg) translateZ(-100px) ;
+}
+
+/* 接下来我给自己准备一个编辑器 */
+.resumeEditor{
+  position: fixed; right: 0; top: 0;
+  padding: .5em;  margin: .5em;
+  width: 48vw; height: 90vh;
+  border: 1px solid;
+  background: white; color: #222;
+  overflow: auto;
+}
+/* 好了，我开始写简历了 */
+
+
+`,
+          `
+/* 这个简历好像差点什么
+ * 对了，这是 Markdown 格式的，我需要变成对 HR 更友好的格式
+ * 简单，用开源工具翻译成 HTML 就行了
+ */
+`
+          ,
+          `
+/* 再对 HTML 加点样式 */
+.resumeEditor{
+  padding: 2em;
+}
+.resumeEditor h2{
+  display: inline-block;
+  border-bottom: 1px solid;
+  margin: 1em 0 .5em;
+}
+.resumeEditor ul,.resumeEditor ol{
+  list-style: none;
+}
+.resumeEditor ul> li::before{
+  content: '•';
+  margin-right: .5em;
+}
+.resumeEditor ol {
+  counter-reset: section;
+}
+.resumeEditor ol li::before {
+  counter-increment: section;
+  content: counters(section, ".") " ";
+  margin-right: .5em;
+}
+.resumeEditor blockquote {
+  margin: 1em;
+  padding: .5em;
+  background: #ddd;
+}
+`],
+        currentMarkdown: '',
+        fullMarkdown: `肖振辉
+----
+
++86 15806259460 | innopeaceshaw@gmail.com
+web前端工程师
+
+技能
+----
+
+* 熟悉掌握HTML、CSS、JavaScript,可以脱离框架进行独立开发；
+* 熟悉掌握Vue及相关的全家桶；
+* 了解TypeScript;
+* 可以使用Krpano+JavaScript进行全景开发；
+* 使用过Vue2+Vuecli+webpack+ElementUI+Chartjs 编写公司所有项目链接后台管理系统
+
+项目经历
+----
+
+* 小猪全景模版开发
+项目概述：实现一个可复用性的全景模版，供公司批量生成全景项目。
+负责内容：
+1.负责项目PC+Mobile同时上线；
+2.负责组内开发完成后的测试及完善；
+3.负责项目的前端性能优化及后期维护；
+项目URL：
+http://pano.818qj.com/xiaozhu_test/20332593103/index.html
+
+工作经历
+----
+
+* 2015.9 - 2017.1（苏州新看点信息技术有限公司担任全景摄影师）
+* 2017.2 - 2018.11（苏州新看点信息技术有限公司担任web前端工程师后期兼任全景封装组长）
+
+教育背景
+----
+
+* 2011.9 - 2014.6（江西工业职业技术学院 信息工程系 软件技术）
+
+链接
+----
+
+* [GitHub](https://github.com/JefferyShaw)
+
+
+`
+      }
+    },
+    created() {
+      this.makeResume()
+    },
+
+    methods: {
+      makeResume: async function () {
+        await this.progressivelyShowStyle(0)
+        await this.progressivelyShowResume()
+        await this.progressivelyShowStyle(1)
+        await this.showHtml()
+        await this.progressivelyShowStyle(2)
+      },
+      showHtml: function () {
+        return new Promise((resolve, reject) => {
+          this.enableHtml = true
+          resolve()
+        })
+      },
+      progressivelyShowStyle(n) {
+        return new Promise((resolve, reject) => {
+          let interval = this.interval
+          let showStyle = (async function () {
+            let style = this.fullStyle[n]
+            if (!style) { return }
+            // 计算前 n 个 style 的字符总数
+            let length = this.fullStyle.filter((_, index) => index <= n).map((item) => item.length).reduce((p, c) => p + c, 0)
+            let prefixLength = length - style.length
+            if (this.currentStyle.length < length) {
+              let l = this.currentStyle.length - prefixLength
+              let char = style.substring(l, l + 1) || ' '
+              this.currentStyle += char
+              if (style.substring(l - 1, l) === '\n' && this.$refs.styleEditor) {
+                this.$nextTick(() => {
+                  this.$refs.styleEditor.goBottom()
+                })
+              }
+              setTimeout(showStyle, interval)
+            } else {
+              resolve()
+            }
+          }).bind(this)
+          showStyle()
+        })
+      },
+      progressivelyShowResume() {
+        return new Promise((resolve, reject) => {
+          let length = this.fullMarkdown.length
+          let interval = this.interval
+          let showResume = () => {
+            if (this.currentMarkdown.length < length) {
+              this.currentMarkdown = this.fullMarkdown.substring(0, this.currentMarkdown.length + 1)
+              let lastChar = this.currentMarkdown[this.currentMarkdown.length - 1]
+              let prevChar = this.currentMarkdown[this.currentMarkdown.length - 2]
+              if (prevChar === '\n' && this.$refs.resumeEditor) {
+                this.$nextTick(() => this.$refs.resumeEditor.goBottom())
+              }
+              setTimeout(showResume, interval)
+            } else {
+              resolve()
+            }
+          }
+          showResume()
+        })
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html {
+    min-height: 100vh;
+  }
+  *{
+    box-sizing: border-box;
+  }
+</style>
